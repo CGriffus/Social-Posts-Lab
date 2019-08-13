c4ab058c174e6727d81c5e63a91389ebfd32e2f3
Caitlin <caitlin@griffus.com>
2019-08-13 16:06:50 -0400

    added for loop and some form binding

--- src/app/social-posts/post-form/post-form.component.ts
+++ src/app/social-posts/post-form/post-form.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-post-form',
-  templateUrl: './post-form.component.html',
-  styleUrls: ['./post-form.component.css']
+  selector: "app-post-form",
+  templateUrl: "./post-form.component.html",
+  styleUrls: ["./post-form.component.css"]
 })
 export class PostFormComponent implements OnInit {
+  @Output() onAdd = new EventEmitter<any>();
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  addPost(postForm: NgForm): void {
+    console.log(postForm.value.title);
+    // this.onAdd.emit({
+    //   title: postForm.value.title,
+    //   thought: PostFormComponent.value.title
+    // });
   }
 
+  ngOnInit() {}
 }
--- src/app/social-posts/social-posts.component.ts
+++ src/app/social-posts/social-posts.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Post } from "../interfaces/post";
 
 @Component({
-  selector: 'app-social-posts',
-  templateUrl: './social-posts.component.html',
-  styleUrls: ['./social-posts.component.css']
+  selector: "app-social-posts",
+  templateUrl: "./social-posts.component.html",
+  styleUrls: ["./social-posts.component.css"]
 })
 export class SocialPostsComponent implements OnInit {
+  show: boolean = false;
 
-  constructor() { }
+  posts: Post[] = [
+    { title: "My new post", thought: "blah blah blah" },
+    { title: "Another new post", thought: "bleh bleh blah" }
+  ];
 
-  ngOnInit() {
+  constructor() {}
+
+  addNewPostHandler(newPost: object): void {
+    // console.log("added post");
+    this.posts.unshift(newPost);
   }
 
+  removePostHandler() {}
+
+  toggleFormHandler() {}
+
+  ngOnInit() {}
 }
